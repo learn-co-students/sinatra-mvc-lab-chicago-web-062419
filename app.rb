444fffc3dda58fd733ed1112f425c2cea29c17f0
christianalim <christianalim1@gmail.com>
2019-07-15 15:58:11 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+
+        erb :index
+    end
+
+    post '/piglatinize' do 
+        
+        erb :user_input
+    end
+
 end
+
