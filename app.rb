fd7b60c1ed6ef6437d0224006bfec38bc243d3d6
srwatlanta <shawnrwheeler@gmail.com>
2019-07-16 18:29:33 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
-require_relative 'config/environment'
+require_relative "config/environment"
 
 class App < Sinatra::Base
+  get "/" do
+    erb :user_input
+  end
+
+  post "/piglatinize" do
+    @new = PigLatinizer.new
+    @params = params[:user_phrase]
+    erb :finished_phrase
+  end
 end
